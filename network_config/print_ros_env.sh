692a5a914f54c1a596d33fbf5bfbf3333c28b605
treyfortmuller <tfortmuller@berkeley.edu>
2019-04-20 19:14:50 -0700

    ros network config scripts

---
+++ network_config/print_ros_env.sh
@@ -0,0 +1,6 @@
+# for confirming the ROS environment variables
+# for networking many machines together
+
+echo "ROS_MASTER_URI:" $ROS_MASTER_URI
+echo "ROS_HOSTNAME:" $ROS_HOSTNAME
+echo "ROS_IP:" $ROS_IP
---
+++ network_config/set_ros_env.sh
@@ -0,0 +1,5 @@
+# for setting the ros environment variables for networking
+# many machines together - makes trey's laptop master
+
+export ROS_MASTER_URI=http://trey-thinkpad:11311
+export ROS_HOSTNAME=$(hostname)
