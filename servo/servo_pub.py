b4eaee5e6d83339377ca1c755f46fbdd4707c7e3
treyfortmuller <tfortmuller@berkeley.edu>
2019-05-02 21:20:53 -0700

    servo publisher and listener done but untested

--- servo/servo_pub.py
+++ servo/servo_pub.py
@@ -1,47 +1,25 @@
 #!/usr/bin/env python
 '''
-A script for actuating a small hobby servo using the GPIO pins on a raspberry pi
-which subscribes to a ROS topic for actuation commands.
+A ROS node for publishing duty cycle commands to the "claw" topic for actuating
+the servo driven claw on the v2v quadcopters. User is prompted for a new command which is
+immediately published to the "claw"t opic
 
 TODO: this is untested! - Trey
-
 '''
 import rospy
 from std_msgs.msg import Float32
 
-import RPi.GPIO as GPIO
-import time
+def talker():
+    pub = rospy.Publisher('claw', Float32, queue_size=10)
+    rospy.init_node('servo_pub', anonymous=True)
 
-DUTY_MAX = 12.5
-DUTY_MIN = 2.5
-
-DUTY_CENTER = (DUTY_MIN + DUTY_MAX)/2
-
-servo_pin = 12 # physical pin number of RPi
-
-# setup the GPIO pin
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(servo_pin, GPIO.OUT)
-
-p = GPIO.PWM(servo_pin, 50) # start PWM output on the servo pin at 50Hz
-p.start(DUTY_CENTER) # center the servo on initialization
-
-def callback(data):
-    duty_cycle_cmd = data.data
-
-    # debug output
-    rospy.loginfo("commanding servo to " + duty_cycle_cmd + "\% duty cycle")
-
-    p.ChangeDutyCycle(duty_cycle_cmd)
-    
-def listener():
-    rospy.init_node('servo_listener', anonymous=True)
-
-    # calls the call back every time a message is published to the "claw" topic
-    rospy.Subscriber("claw", Float32, callback)
-
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+    while not rospy.is_shutdown():
+        # get user input for the new duty cycle command
+        new_cmd = float(input("new servo duty cycle? "))
+        pub.publish(new_cmd)
 
 if __name__ == '__main__':
-    listener()
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
